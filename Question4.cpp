088c22ce159f4d21add803f88527910c6a858d5b
BusK <kmabusha2@gmail.com>
2016-03-30 05:50:25 +0200

    base class Animal, derived classes

--- Question4.cpp
+++ Question4.cpp
@@ -1,8 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //                         COMPUTER METHODS 3 PRACTICAL TEST 1 (MOCK)
-// Name:
-// Student No:
-// Date: 
+// Name:Kabo Onesmo Mabusha
+// Student No:214576451
+// Date: 2016/03/30
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // ***** QUESTION 4 ****
@@ -11,3 +14,43 @@
 // 4.2 From class "Animal", derive a concrete class "Bird" and "Dog".  Print appropriate message for
 //     each method.
 // 4.3 Write a driver program which creates an object of each type and demonstrates POLYMORPHISM. 
+class Animal{
+public:
+	virtual void name(){}
+	virtual void does(){}
+};
+class Bird: public Animal{
+public:
+	Bird(){}
+      void name(){
+		cout << "I am Bird" << endl;
+	}
+	 void does(){
+		cout << "I go chirp chirp" << endl;
+	}
+};
+class Dog : public Animal{
+public:
+	Dog(){}
+	 void name(){
+		cout << "I am Dog" << endl;
+	}
+	 void does(){
+		cout << "I go woof woof" << endl;
+	}
+};
+
+int main(){
+	Animal *AnimalPtr;
+	Dog theDog;
+	Bird theBird;
+
+	AnimalPtr = &theBird;
+	AnimalPtr->name();
+	AnimalPtr->does();
+
+	AnimalPtr = &theDog;
+	AnimalPtr->name();
+	AnimalPtr->does();
+
+};
